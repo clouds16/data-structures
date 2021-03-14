0ff463f6f2883419cab6c53abf47ddf55b1bc6a2
Hector <hect16@gmail.com>
2021-03-13 16:56:52 -0800

    week1 updates, final submission

--- week1/is_multiple.py
+++ week1/is_multiple.py
@@ -1,14 +1,19 @@
 
 def isMultiple(n, m):
+    #will check if there are any remainders, if there are no remainders , values are multiples
     if n % m == 0 :
         return True
+    #values are not multiples, returns a false
     else: 
         return False
 
 def main():
-    value  = isMultiple(int(input("Enter first value: ")), int(input("Enter second value: ")))
-    if value :
+    #will prompt the user for a multiples, multiples should be an integer 
+    multiples  = isMultiple(int(input("Enter first multiples: ")), int(input("Enter second multiples: ")))
+    #if multiples is true, print to prompt that the program found multiples
+    if multiples :
         print("Is a multiple!")
+    #print that no multiples were found 
     else:
         print("Is not a multiple!")
 
--- week1/punishment.py
+++ week1/punishment.py
@@ -2,12 +2,16 @@
 def punishment( repetition):
     punishment_line = "I will never spam my friends again!"
     
+    #while loop will run until t he repetition value is 0 
     while repetition > 0 :
         print(punishment_line)
+        #iterator that will update the repetition value
         repetition -= 1
         
-
+#main loop to separate the concerns - will execute program
 def main():
-    punishment(int(input("How many times will the line be repeated: ")))
+    #punishment(int(input("How many times will the line be repeated: ")))
+    
+    punishment(100)
 
 main()
