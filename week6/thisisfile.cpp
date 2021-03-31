6428635ef86bf9dc2498ae33fbd68b4a029df5be
hect <hect16@gmail.com>
2021-03-31 13:02:24 -0700

    Week4 edits

---
+++ week6/thisisfile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std ;
+
+int main(){
+    int x;
+    cout << "Input a number" << endl ;
+    cin >> x ;
+    cout << "your number is: " << x << endl;
+    return 0;
+
+}
